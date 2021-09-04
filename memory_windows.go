3dbe9e6f065e8966e5e75b9862382d027b06413f
Jan0660 <jan0660@tutanota.com>
2021-09-04 14:11:30 +0000

    implement Windows swap

--- memory_windows.go
+++ memory_windows.go
@@ -22,23 +22,23 @@ type memStatusEx struct {
 	ullAvailExtendedVirtual uint64
 }
 
-func getMemoryStatus() (memStatusEx, error) {
+func getMemoryStatus() (*memStatusEx, error) {
 	kernel32, err := syscall.LoadDLL("kernel32.dll")
 	if err != nil {
-		return nil, errors.new("Failed to load kernel32.dll")
+		return nil, errors.New("Failed to load kernel32.dll")
 	}
 	// GetPhysicallyInstalledSystemMemory is simpler, but broken on
 	// older versions of windows (and uses this under the hood anyway).
 	globalMemoryStatusEx, err := kernel32.FindProc("GlobalMemoryStatusEx")
 	if err != nil {
-		return nil, errors.new()
+		return nil, errors.New("Couldn't find procedure")
 	}
 	msx := &memStatusEx{
 		dwLength: 64,
 	}
 	r, _, _ := globalMemoryStatusEx.Call(uintptr(unsafe.Pointer(msx)))
 	if r == 0 {
-		return nil, errors.new("Failed to call Windows API")
+		return nil, errors.New("Failed to call Windows API")
 	}
 	return msx, nil
 }
@@ -59,14 +59,20 @@ func sysFreeMemory() uint64 {
 	return msx.ullAvailPhys
 }
 
-// Not implemented
 func sysTotalSwap() uint64 {
-	return 0
+	msx, err := getMemoryStatus()
+	if err != nil {
+		return 0
+	}
+	return msx.ullTotalPageFile
 }
 
-// Not implemented
 func sysFreeSwap() uint64 {
-	return 0
+	msx, err := getMemoryStatus()
+	if err != nil {
+		return 0
+	}
+	return msx.ullAvailPageFile
 }
 
 func sysAvailableMemory() uint64 {
